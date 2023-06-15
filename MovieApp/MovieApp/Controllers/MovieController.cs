3849332763b3f3a6b11006423fc95503ec0ba0ce
LojainHassan <lojainhteabat@gmail.com>
2023-06-15 23:44:11 +0300

    first initially to repository

---
+++ MovieApp/MovieApp/Controllers/MovieController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApp.Models;
+using Microsoft.EntityFrameworkCore;
+namespace MovieApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly MovieContext movieContext;
+
+        public MovieController(MovieContext movieContext)
+        {
+            this.movieContext = movieContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            if (movieContext.Movies == null)
+            {
+                return BadRequest();
+            }
+            return await movieContext.Movies.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            if (movieContext.Movies == null)
+            {
+                return BadRequest();
+            }
+            return await movieContext.Movies.FindAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Movie>> AddMovie(Movie movie)
+        {
+            if(movie == null)
+            {
+                return BadRequest();
+            }
+            movieContext.AddAsync(movie);
+            movieContext.SaveChanges();
+            return await movieContext.Movies.FindAsync(movie.Id);
+        }
+
+        [HttpDelete]
+        public async Task  DeleteMovie(int id)
+        {
+            movieContext.Remove(id);
+            movieContext.SaveChanges();
+            
+        }
+
+
+
+    }
+}
